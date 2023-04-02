628624051ffd85653368eabee64258703afbf5b0
HamnYce <hamadboa@gmail.com>
2023-04-02 23:57:01 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Jaya Implementation in C
---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+typedef struct {
+  double x;
+  double y;
+} sol;
+
+
+int main() {
+  printf("Hello World");
+  printf("%f", rand());
+
+  return 0;
+}
